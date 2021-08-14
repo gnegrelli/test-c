a546ddf1556a4f03a007d130b6baed3d31e141b4
Gabriel Negrelli <gnegrelli13@gmail.com>
2021-08-13 22:05:16 -0300

    Upload results of question 9

---
+++ question_9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void){
+    char *s = NULL;
+    printf("Enter string to be measured:\n");
+    scanf("%m[^\n]s", &s);
+    if (s == NULL){
+        printf("Error while retrievin input");
+    }
+    else{
+        int i = 0;
+        while (s[i] != '\0'){
+            i++;
+        }
+        printf("String \"%s\" is %d characters long.", s, i);
+    }
+}
